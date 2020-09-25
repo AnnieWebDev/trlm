c64c327ad6e1aa25479b94d64b741ed699d2e888
AnnieWebDev <komochek2013@gmail.com>
2020-09-25 16:54:04 +0300

    верстка футера, добавление слайдера

---
+++ js/dist/main.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  navigation: {
+    nextEl: '.slider__navigation-link_next',
+    prevEl: '.slider__navigation-link_prev'
+  }
+});
---
+++ js/main.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: false,
+    navigation: {
+        nextEl: '.slider__navigation-link_next',
+        prevEl: '.slider__navigation-link_prev',
+    }
+})
